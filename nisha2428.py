8fa2c1808cb9fa3bf865b3b3cb4c0bd6027c82c6
NishaSpn <nishaspn2428@gmail.com>
2026-08-03 12:21:57 +0530

    nisha2428.py

--- nisha2428.py
+++ nisha2428.py
@@ -1,1 +1,65 @@
-# DS-lab
+class Node:
+  def init (self, data):
+    self.data =
+    data self.next =
+    None
+class LinkedList:
+  def    init (self):
+    self.head = None
+    
+  def push(self, new_data):
+    new_node = 
+    Node(new_data) 
+    new_node.next = 
+    self.head self.head = 
+    new_node 
+def insertAfter(self, prev_node,
+     new_data): if prev_node is None:
+       print("The given previous node must  inLinkedList.")return
+
+    new_node = Node(new_data)
+    new_node.next=
+    prev_node.next
+    prev_node.next  =new_node
+                 
+def append(self,new_data):
+  new_node=
+  Node(new_data)if
+  self.head is None:
+         self.head=
+         new_node return
+  
+  last=self.head
+  while
+  (last.next):
+    last=last.next
+    last.next=new_node
+                   
+def printList(self):
+  temp=
+  self.haed while
+  (temp):
+    print(temp.data)
+    temp=
+    temp.next
+    
+    
+if_name_=='_main_':
+    llist=LinkedList() 
+    llist.append(6)
+    llist.push(7);
+llist.push(1);
+llist.append(4)
+llist.insertAfter(llist.head.next,8)
+print('Created linked list is:')
+llist.printList()
+
+      
+
+
+                   
+
+          
+            
+ 
+
